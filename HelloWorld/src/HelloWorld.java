02e297ce9596229580a6e24b9fc0079350fe1a09
liangzhu198 <brightzhuliang@gmail.com>
2013-09-18 16:47:11 -0700

    conflict solved

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-
 public class HelloWorld {
 	public static void main( String [] args){
 		System.out.println("Hello Welt!");
 	}
 
 }
-=======
 
-public class HelloWorld {
-	public static void main( String [] args){
-		System.out.println("Hello World!");
-	}
-
-}
->>>>>>> 62c135c... copy files to repo
